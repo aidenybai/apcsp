c7fad9fd921c7e1552fbeefda3397166088391d9
Aiden Bai <aiden.bai05@gmail.com>
2021-09-08 09:50:12 -0700

    password-generator 1631119773548

---
+++ password-generator/README.md
@@ -0,0 +1,5 @@
+# [password-generator](https://apcsp.netlify.app/password-generator)
+
+Run `pnpm bump` to push to github
+
+_Created by Aiden Bai on Wed Sep 08 2021 (1631119773548)_
---
+++ password-generator/index.js
@@ -0,0 +1,30 @@
+// 1. Reverse the name of the service.
+// 2. Lowercase the reversed name.
+// 3. Truncate the first and last character of the lowercased reversed name to produce an abbreviation.
+// 4. Type the abbreviation.
+// 5. Replace every "e" in the abbreviation with the number "3".
+// 6. Replace every "a" in the abbreviation with the number "4".
+// 7. Replace every "i" in the abbreviation with the number "1".
+// 8. Replace every "o" in the abbreviation with the number "0".
+// 8. Capitalize every vowel in the abbreviation.
+// 8. Append the reversed abbreviation to the end of the site abbreviation.
+// 9. Append the same number of "!" as the length of the abbreviation to the end of the site abbreviation.
+// 10. Insert the same number of "!" as the length of the abbreviation to the start of the site abbreviation.
+
+const passwordGenerator = (serviceName) => {
+  let abbr = serviceName.toLowerCase().split('').reverse();
+  abbr.pop();
+  abbr.shift();
+  abbr = abbr.join('');
+  abbr = abbr.replace(/e/g, '3');
+  abbr = abbr.replace(/a/g, '4');
+  abbr = abbr.replace(/i/g, '1');
+  abbr = abbr.replace(/o/g, '0');
+  abbr = abbr.replace(/[aeiou]/g, (vowel) => vowel.toUpperCase());
+  abbr += abbr.split('').reverse().join('');
+  abbr += '!'.repeat(abbr.length);
+  abbr = '!'.repeat(abbr.length) + abbr;
+  return abbr;
+};
+
+console.log(passwordGenerator('Twitter'));
